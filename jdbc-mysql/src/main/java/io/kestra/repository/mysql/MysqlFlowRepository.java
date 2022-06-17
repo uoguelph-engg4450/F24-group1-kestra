kestra-io/kestra

feat(jdbc): first implementation of mysql & postgres repository

repository-mysql/src/main/java/io/kestra/repository/mysql/MysqlFlowRepository.java
@@ -1,0 +1,75 @@
+package io.kestra.repository.mysql;
+
+import io.kestra.core.models.SearchResult;
+import io.kestra.core.models.flows.Flow;
+import io.kestra.core.repositories.ArrayListTotal;
+import io.kestra.jdbc.repository.AbstractFlowRepository;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.data.model.Pageable;
+import org.jooq.*;
+import org.jooq.impl.DSL;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+@MysqlRepositoryEnabled
+public class MysqlFlowRepository extends AbstractFlowRepository {
+    @Inject
+    public MysqlFlowRepository(ApplicationContext applicationContext) {
+        super(new MysqlRepository<>(Flow.class, applicationContext), applicationContext);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <R extends Record> SelectConditionStep<R> fullTextSelect(List<Field<Object>> field) {
+        ArrayList<Field<Object>> fields = new ArrayList<>(Collections.singletonList(DSL.field("value")));
+
+        if (field != null) {
+            fields.addAll(field);
+        }
+
+        return (SelectConditionStep<R>) this.jdbcRepository
+            .getDslContext()
+            .select(fields)
+            .hint("SQL_CALC_FOUND_ROWS")
+            .from(lastRevision(false))
+            .join(jdbcRepository.getTable().as("ft"))
+            .on(
+                DSL.field("ft.key").eq(DSL.field("rev.key"))
+                    .and(DSL.field("ft.revision").eq(DSL.field("rev.revision")))
+            )
+            .where(this.defaultFilter());
+    }
+
+    public ArrayListTotal<Flow> find(String query, Pageable pageable) {
+        SelectConditionStep<Record1<Object>> select = this.fullTextSelect(Collections.emptyList());
+
+        if (query != null) {
+            select.and(this.jdbcRepository.fullTextCondition(Arrays.asList("namespace", "id"), query));
+        }
+
+        return this.jdbcRepository.fetchPage(select, pageable);
+    }
+
+    @Override
+    public ArrayListTotal<SearchResult<Flow>> findSourceCode(String query, Pageable pageable) {
+        SelectConditionStep<Record> select = this.fullTextSelect(Collections.singletonList(DSL.field("source_code")));
+
+        if (query != null) {
+            select.and(this.jdbcRepository.fullTextCondition(Collections.singletonList("source_code"), query));
+        }
+
+        return this.jdbcRepository.fetchPage(
+            select,
+            pageable,
+            record -> new SearchResult<>(
+                this.jdbcRepository.map(record),
+                this.jdbcRepository.fragments(query, record.getValue("source_code", String.class))
+            )
+        );
+    }
+}

repository-postgres/src/main/java/io/kestra/repository/postgres/PostgresFlowRepository.java
@@ -1,0 +1,73 @@
+package io.kestra.repository.postgres;
+
+import io.kestra.core.models.SearchResult;
+import io.kestra.core.models.flows.Flow;
+import io.kestra.core.repositories.ArrayListTotal;
+import io.kestra.jdbc.repository.AbstractFlowRepository;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.data.model.Pageable;
+import org.jooq.*;
+import org.jooq.impl.DSL;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+@PostgresRepositoryEnabled
+public class PostgresFlowRepository extends AbstractFlowRepository {
+    @Inject
+    public PostgresFlowRepository(ApplicationContext applicationContext) {
+        super(new PostgresRepository<>(Flow.class, applicationContext), applicationContext);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <R extends Record, E> SelectConditionStep<R> fullTextSelect(List<Field<Object>> field) {
+        ArrayList<Field<Object>> fields = new ArrayList<>(Collections.singletonList(DSL.field("value")));
+
+        if (field != null) {
+            fields.addAll(field);
+        }
+
+        return (SelectConditionStep<R>) this.jdbcRepository
+            .getDslContext()
+            .select(fields)
+            .from(lastRevision(false))
+            .join(jdbcRepository.getTable().as("ft"))
+            .on(
+                DSL.field("ft.key").eq(DSL.field("rev.key"))
+                    .and(DSL.field("ft.revision").eq(DSL.field("rev.revision")))
+            )
+            .where(this.defaultFilter());
+    }
+
+    public ArrayListTotal<Flow> find(String query, Pageable pageable) {
+        SelectConditionStep<Record1<Object>> select = this.fullTextSelect(Collections.emptyList());
+
+        if (query != null) {
+            select.and(this.jdbcRepository.fullTextCondition(Collections.singletonList("fulltext"), query));
+        }
+
+        return this.jdbcRepository.fetchPage(select, pageable);
+    }
+
+    @Override
+    public ArrayListTotal<SearchResult<Flow>> findSourceCode(String query, Pageable pageable) {
+        SelectConditionStep<Record> select = this.fullTextSelect(Collections.singletonList(DSL.field("source_code")));
+
+        if (query != null) {
+            select.and(DSL.condition("source_code @@ TO_TSQUERY('simple', ?)", query));
+        }
+
+        return this.jdbcRepository.fetchPage(
+            select,
+            pageable,
+            record -> new SearchResult<>(
+                this.jdbcRepository.map(record),
+                this.jdbcRepository.fragments(query, record.getValue("value", String.class))
+            )
+        );
+    }
+}

jdbc/src/main/java/io/kestra/jdbc/repository/AbstractTemplateRepository.java
@@ -1,0 +1,123 @@
+package io.kestra.jdbc.repository;
+
+import io.kestra.core.events.CrudEvent;
+import io.kestra.core.events.CrudEventType;
+import io.kestra.core.models.templates.Template;
+import io.kestra.core.queues.QueueFactoryInterface;
+import io.kestra.core.queues.QueueInterface;
+import io.kestra.core.repositories.TemplateRepositoryInterface;
+import io.kestra.jdbc.AbstractJdbcRepository;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.event.ApplicationEventPublisher;
+import io.micronaut.inject.qualifiers.Qualifiers;
+import org.jooq.Record1;
+import org.jooq.Select;
+import org.jooq.SelectConditionStep;
+import org.jooq.impl.DSL;
+
+import java.util.List;
+import java.util.Optional;
+import javax.inject.Singleton;
+import javax.validation.ConstraintViolationException;
+
+@Singleton
+public abstract class AbstractTemplateRepository extends AbstractRepository implements TemplateRepositoryInterface {
+    private final QueueInterface<Template> templateQueue;
+    private final ApplicationEventPublisher eventPublisher;
+    protected AbstractJdbcRepository<Template> jdbcRepository;
+
+    @SuppressWarnings("unchecked")
+    public AbstractTemplateRepository(AbstractJdbcRepository<Template> jdbcRepository, ApplicationContext applicationContext) {
+        this.jdbcRepository = jdbcRepository;
+        this.eventPublisher = applicationContext.getBean(ApplicationEventPublisher.class);
+        this.templateQueue = applicationContext.getBean(QueueInterface.class, Qualifiers.byName(QueueFactoryInterface.TEMPLATE_NAMED));
+    }
+
+    @Override
+    public Optional<Template> findById(String namespace, String id) {
+        Select<Record1<Object>> from = jdbcRepository.getDslContext().select(DSL.field("value"))
+            .from(this.jdbcRepository.getTable())
+            .where(this.defaultFilter())
+            .and(DSL.field("namespace").eq(namespace))
+            .and(DSL.field("id").eq(id));
+
+        return this.jdbcRepository.fetchOne(from);
+    }
+
+    @Override
+    public List<Template> findAll() {
+        SelectConditionStep<Record1<Object>> select = this.jdbcRepository
+            .getDslContext()
+            .select(DSL.field("value"))
+            .from(this.jdbcRepository.getTable())
+            .where(this.defaultFilter());
+
+        return this.jdbcRepository.fetch(select);
+    }
+
+    @Override
+    public List<Template> findByNamespace(String namespace) {
+        SelectConditionStep<Record1<Object>> select = this.jdbcRepository
+            .getDslContext()
+            .select(DSL.field("value"))
+            .from(this.jdbcRepository.getTable())
+            .where(DSL.field("namespace").eq(namespace))
+            .and(this.defaultFilter());
+
+        return this.jdbcRepository.fetch(select);
+    }
+
+    @Override
+    public Template create(Template template) throws ConstraintViolationException {
+        this.jdbcRepository.persist(template);
+
+        templateQueue.emit(template);
+        eventPublisher.publishEvent(new CrudEvent<>(template, CrudEventType.CREATE));
+
+        return template;
+    }
+
+    public Template update(Template template, Template previous) throws ConstraintViolationException {
+        this
+            .findById(previous.getNamespace(), previous.getId())
+            .map(current -> current.validateUpdate(template))
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .ifPresent(s -> {
+                throw s;
+            });
+
+        this.jdbcRepository.persist(template);
+
+        templateQueue.emit(template);
+        eventPublisher.publishEvent(new CrudEvent<>(template, CrudEventType.UPDATE));
+
+        return template;
+    }
+
+    @Override
+    public void delete(Template template) {
+        if (this.findById(template.getNamespace(), template.getId()).isEmpty()) {
+            throw new IllegalStateException("Template " + template.getId() + " doesn't exists");
+        }
+
+        Template deleted = template.toDeleted();
+
+        this.jdbcRepository.persist(deleted);
+
+        templateQueue.emit(deleted);
+        eventPublisher.publishEvent(new CrudEvent<>(deleted, CrudEventType.DELETE));
+    }
+
+    @Override
+    public List<String> findDistinctNamespace() {
+        return this.jdbcRepository
+            .getDslContext()
+            .select(DSL.field("namespace"))
+            .from(this.jdbcRepository.getTable())
+            .where(this.defaultFilter())
+            .groupBy(DSL.grouping(DSL.field("namespace")))
+            .fetch()
+            .map(record -> record.getValue("namespace", String.class));
+    }
+}

jdbc/src/test/java/io/kestra/jdbc/JdbcTestUtils.java
@@ -1,0 +1,46 @@
+package io.kestra.jdbc;
+
+import com.zaxxer.hikari.HikariConfig;
+import io.micronaut.flyway.FlywayConfigurationProperties;
+import io.micronaut.flyway.FlywayMigrator;
+import lombok.SneakyThrows;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.sql.DataSource;
+
+import static io.kestra.core.utils.Rethrow.throwPredicate;
+
+@Singleton
+public class JdbcTestUtils {
+    @Inject
+    private DSLContext dslContext;
+
+    @Inject
+    private FlywayMigrator flywayMigrator;
+
+    @Inject
+    private FlywayConfigurationProperties config;
+
+    @Inject
+    private DataSource dataSource;
+
+    @SneakyThrows
+    public void drop() {
+        dslContext.meta()
+            .getTables()
+            .stream()
+            .filter(throwPredicate(table ->
+                (dslContext.dialect() == SQLDialect.MYSQL && table.getSchema().getName().equals(dataSource.getConnection().getCatalog())) ||
+                dslContext.dialect() != SQLDialect.MYSQL
+            ))
+            .filter(table -> !table.getName().equals("flyway_schema_history"))
+            .forEach(t -> dslContext.truncate(t.getName()).execute());
+    }
+
+    public void migrate() {
+        flywayMigrator.run(config, dataSource);
+    }
+}

jdbc/src/test/java/io/kestra/jdbc/repository/AbstractJdbcFlowRepositoryTest.java
@@ -1,0 +1,59 @@
+package io.kestra.jdbc.repository;
+
+import io.kestra.core.Helpers;
+import io.kestra.core.models.SearchResult;
+import io.kestra.core.models.flows.Flow;
+import io.kestra.jdbc.JdbcTestUtils;
+import io.micronaut.data.model.Pageable;
+import io.micronaut.data.model.Sort;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import javax.inject.Inject;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+
+public abstract class AbstractJdbcFlowRepositoryTest extends io.kestra.core.repositories.AbstractFlowRepositoryTest {
+    @Inject
+    AbstractFlowRepository flowRepository;
+
+    @Inject
+    JdbcTestUtils jdbcTestUtils;
+
+    @Test
+    void find() {
+        List<Flow> save = flowRepository.find(null, Pageable.from(1, 100, Sort.of(Sort.Order.asc("id"))));
+        assertThat((long) save.size(), is(Helpers.FLOWS_COUNT));
+
+        save = flowRepository.find("trigger-multiplecondition", Pageable.from(1, 10, Sort.UNSORTED));
+        assertThat((long) save.size(), is(3L));
+    }
+
+    @Test
+    void findSourceCode() {
+        List<SearchResult<Flow>> search = flowRepository.findSourceCode("io.kestra.core.models.conditions.types.MultipleCondition", Pageable.from(1, 10, Sort.UNSORTED));
+
+        assertThat((long) search.size(), is(1L));
+
+        SearchResult<Flow> flow = search
+            .stream()
+            .filter(flowSearchResult -> flowSearchResult.getModel()
+                .getId()
+                .equals("trigger-multiplecondition-listener"))
+            .findFirst()
+            .orElseThrow();
+        assertThat(flow.getFragments().get(0), containsString("types.MultipleCondition</mark>"));
+    }
+
+    @BeforeEach
+    protected void init() throws IOException, URISyntaxException {
+        jdbcTestUtils.drop();
+        jdbcTestUtils.migrate();
+        super.init();
+    }
+}

uoguelph-engg4450/F24-group1-kestra

jdbc-mysql/src/main/java/io/kestra/repository/mysql/MysqlFlowRepository.java
@@ -1,0 +1,78 @@
+package io.kestra.repository.mysql;
+
+import io.kestra.core.models.SearchResult;
+import io.kestra.core.models.flows.Flow;
+import io.kestra.core.repositories.ArrayListTotal;
+import io.kestra.jdbc.repository.AbstractFlowRepository;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.data.model.Pageable;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import org.jooq.Field;
+import org.jooq.Record;
+import org.jooq.Record1;
+import org.jooq.SelectConditionStep;
+import org.jooq.impl.DSL;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Singleton
+@MysqlRepositoryEnabled
+public class MysqlFlowRepository extends AbstractFlowRepository {
+    @Inject
+    public MysqlFlowRepository(ApplicationContext applicationContext) {
+        super(new MysqlRepository<>(Flow.class, applicationContext), applicationContext);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <R extends Record> SelectConditionStep<R> fullTextSelect(List<Field<Object>> field) {
+        ArrayList<Field<Object>> fields = new ArrayList<>(Collections.singletonList(DSL.field("value")));
+
+        if (field != null) {
+            fields.addAll(field);
+        }
+
+        return (SelectConditionStep<R>) this.jdbcRepository
+            .getDslContext()
+            .select(fields)
+            .hint("SQL_CALC_FOUND_ROWS")
+            .from(lastRevision(false))
+            .join(jdbcRepository.getTable().as("ft"))
+            .on(
+                DSL.field("ft.key").eq(DSL.field("rev.key"))
+                    .and(DSL.field("ft.revision").eq(DSL.field("rev.revision")))
+            )
+            .where(this.defaultFilter());
+    }
+
+    public ArrayListTotal<Flow> find(String query, Pageable pageable) {
+        SelectConditionStep<Record1<Object>> select = this.fullTextSelect(Collections.emptyList());
+
+        if (query != null) {
+            select.and(this.jdbcRepository.fullTextCondition(Arrays.asList("namespace", "id"), query));
+        }
+
+        return this.jdbcRepository.fetchPage(select, pageable);
+    }
+
+    @Override
+    public ArrayListTotal<SearchResult<Flow>> findSourceCode(String query, Pageable pageable) {
+        SelectConditionStep<Record> select = this.fullTextSelect(Collections.singletonList(DSL.field("source_code")));
+
+        if (query != null) {
+            select.and(this.jdbcRepository.fullTextCondition(Collections.singletonList("source_code"), query));
+        }
+
+        return this.jdbcRepository.fetchPage(
+            select,
+            pageable,
+            record -> new SearchResult<>(
+                this.jdbcRepository.map(record),
+                this.jdbcRepository.fragments(query, record.getValue("source_code", String.class))
+            )
+        );
+    }
+}

jdbc-postgres/src/main/java/io/kestra/repository/postgres/PostgresFlowRepository.java
@@ -1,0 +1,76 @@
+package io.kestra.repository.postgres;
+
+import io.kestra.core.models.SearchResult;
+import io.kestra.core.models.flows.Flow;
+import io.kestra.core.repositories.ArrayListTotal;
+import io.kestra.jdbc.repository.AbstractFlowRepository;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.data.model.Pageable;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import org.jooq.Field;
+import org.jooq.Record;
+import org.jooq.Record1;
+import org.jooq.SelectConditionStep;
+import org.jooq.impl.DSL;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Singleton
+@PostgresRepositoryEnabled
+public class PostgresFlowRepository extends AbstractFlowRepository {
+    @Inject
+    public PostgresFlowRepository(ApplicationContext applicationContext) {
+        super(new PostgresRepository<>(Flow.class, applicationContext), applicationContext);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <R extends Record, E> SelectConditionStep<R> fullTextSelect(List<Field<Object>> field) {
+        ArrayList<Field<Object>> fields = new ArrayList<>(Collections.singletonList(DSL.field("value")));
+
+        if (field != null) {
+            fields.addAll(field);
+        }
+
+        return (SelectConditionStep<R>) this.jdbcRepository
+            .getDslContext()
+            .select(fields)
+            .from(lastRevision(false))
+            .join(jdbcRepository.getTable().as("ft"))
+            .on(
+                DSL.field("ft.key").eq(DSL.field("rev.key"))
+                    .and(DSL.field("ft.revision").eq(DSL.field("rev.revision")))
+            )
+            .where(this.defaultFilter());
+    }
+
+    public ArrayListTotal<Flow> find(String query, Pageable pageable) {
+        SelectConditionStep<Record1<Object>> select = this.fullTextSelect(Collections.emptyList());
+
+        if (query != null) {
+            select.and(this.jdbcRepository.fullTextCondition(Collections.singletonList("fulltext"), query));
+        }
+
+        return this.jdbcRepository.fetchPage(select, pageable);
+    }
+
+    @Override
+    public ArrayListTotal<SearchResult<Flow>> findSourceCode(String query, Pageable pageable) {
+        SelectConditionStep<Record> select = this.fullTextSelect(Collections.singletonList(DSL.field("source_code")));
+
+        if (query != null) {
+            select.and(DSL.condition("source_code @@ TO_TSQUERY('simple', ?)", query));
+        }
+
+        return this.jdbcRepository.fetchPage(
+            select,
+            pageable,
+            record -> new SearchResult<>(
+                this.jdbcRepository.map(record),
+                this.jdbcRepository.fragments(query, record.getValue("value", String.class))
+            )
+        );
+    }
+}

jdbc/src/main/java/io/kestra/jdbc/repository/AbstractTemplateRepository.java
@@ -1,0 +1,123 @@
+package io.kestra.jdbc.repository;
+
+import io.kestra.core.events.CrudEvent;
+import io.kestra.core.events.CrudEventType;
+import io.kestra.core.models.templates.Template;
+import io.kestra.core.queues.QueueFactoryInterface;
+import io.kestra.core.queues.QueueInterface;
+import io.kestra.core.repositories.TemplateRepositoryInterface;
+import io.kestra.jdbc.AbstractJdbcRepository;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.event.ApplicationEventPublisher;
+import io.micronaut.inject.qualifiers.Qualifiers;
+import jakarta.inject.Singleton;
+import org.jooq.Record1;
+import org.jooq.Select;
+import org.jooq.SelectConditionStep;
+import org.jooq.impl.DSL;
+
+import java.util.List;
+import java.util.Optional;
+import javax.validation.ConstraintViolationException;
+
+@Singleton
+public abstract class AbstractTemplateRepository extends AbstractRepository implements TemplateRepositoryInterface {
+    private final QueueInterface<Template> templateQueue;
+    private final ApplicationEventPublisher<CrudEvent<Template>> eventPublisher;
+    protected AbstractJdbcRepository<Template> jdbcRepository;
+
+    @SuppressWarnings("unchecked")
+    public AbstractTemplateRepository(AbstractJdbcRepository<Template> jdbcRepository, ApplicationContext applicationContext) {
+        this.jdbcRepository = jdbcRepository;
+        this.eventPublisher = applicationContext.getBean(ApplicationEventPublisher.class);
+        this.templateQueue = applicationContext.getBean(QueueInterface.class, Qualifiers.byName(QueueFactoryInterface.TEMPLATE_NAMED));
+    }
+
+    @Override
+    public Optional<Template> findById(String namespace, String id) {
+        Select<Record1<Object>> from = jdbcRepository.getDslContext().select(DSL.field("value"))
+            .from(this.jdbcRepository.getTable())
+            .where(this.defaultFilter())
+            .and(DSL.field("namespace").eq(namespace))
+            .and(DSL.field("id").eq(id));
+
+        return this.jdbcRepository.fetchOne(from);
+    }
+
+    @Override
+    public List<Template> findAll() {
+        SelectConditionStep<Record1<Object>> select = this.jdbcRepository
+            .getDslContext()
+            .select(DSL.field("value"))
+            .from(this.jdbcRepository.getTable())
+            .where(this.defaultFilter());
+
+        return this.jdbcRepository.fetch(select);
+    }
+
+    @Override
+    public List<Template> findByNamespace(String namespace) {
+        SelectConditionStep<Record1<Object>> select = this.jdbcRepository
+            .getDslContext()
+            .select(DSL.field("value"))
+            .from(this.jdbcRepository.getTable())
+            .where(DSL.field("namespace").eq(namespace))
+            .and(this.defaultFilter());
+
+        return this.jdbcRepository.fetch(select);
+    }
+
+    @Override
+    public Template create(Template template) throws ConstraintViolationException {
+        this.jdbcRepository.persist(template);
+
+        templateQueue.emit(template);
+        eventPublisher.publishEvent(new CrudEvent<>(template, CrudEventType.CREATE));
+
+        return template;
+    }
+
+    public Template update(Template template, Template previous) throws ConstraintViolationException {
+        this
+            .findById(previous.getNamespace(), previous.getId())
+            .map(current -> current.validateUpdate(template))
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .ifPresent(s -> {
+                throw s;
+            });
+
+        this.jdbcRepository.persist(template);
+
+        templateQueue.emit(template);
+        eventPublisher.publishEvent(new CrudEvent<>(template, CrudEventType.UPDATE));
+
+        return template;
+    }
+
+    @Override
+    public void delete(Template template) {
+        if (this.findById(template.getNamespace(), template.getId()).isEmpty()) {
+            throw new IllegalStateException("Template " + template.getId() + " doesn't exists");
+        }
+
+        Template deleted = template.toDeleted();
+
+        this.jdbcRepository.persist(deleted);
+
+        templateQueue.emit(deleted);
+        eventPublisher.publishEvent(new CrudEvent<>(deleted, CrudEventType.DELETE));
+    }
+
+    @Override
+    public List<String> findDistinctNamespace() {
+        return this.jdbcRepository
+            .getDslContext()
+            .select(DSL.field("namespace"))
+            .from(this.jdbcRepository.getTable())
+            .where(this.defaultFilter())
+            .groupBy(DSL.grouping(DSL.field("namespace")))
+            .fetch()
+            .map(record -> record.getValue("namespace", String.class));
+    }
+}

jdbc/src/test/java/io/kestra/jdbc/JdbcTestUtils.java
@@ -1,0 +1,46 @@
+package io.kestra.jdbc;
+
+import com.zaxxer.hikari.HikariConfig;
+import io.micronaut.flyway.FlywayConfigurationProperties;
+import io.micronaut.flyway.FlywayMigrator;
+import lombok.SneakyThrows;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import javax.sql.DataSource;
+
+import static io.kestra.core.utils.Rethrow.throwPredicate;
+
+@Singleton
+public class JdbcTestUtils {
+    @Inject
+    private DSLContext dslContext;
+
+    @Inject
+    private FlywayMigrator flywayMigrator;
+
+    @Inject
+    private FlywayConfigurationProperties config;
+
+    @Inject
+    private DataSource dataSource;
+
+    @SneakyThrows
+    public void drop() {
+        dslContext.meta()
+            .getTables()
+            .stream()
+            .filter(throwPredicate(table ->
+                (dslContext.dialect() == SQLDialect.MYSQL && table.getSchema().getName().equals(dataSource.getConnection().getCatalog())) ||
+                dslContext.dialect() != SQLDialect.MYSQL
+            ))
+            .filter(table -> !table.getName().equals("flyway_schema_history"))
+            .forEach(t -> dslContext.truncate(t.getName()).execute());
+    }
+
+    public void migrate() {
+        flywayMigrator.run(config, dataSource);
+    }
+}

jdbc/src/test/java/io/kestra/jdbc/repository/AbstractJdbcFlowRepositoryTest.java
@@ -1,0 +1,59 @@
+package io.kestra.jdbc.repository;
+
+import io.kestra.core.Helpers;
+import io.kestra.core.models.SearchResult;
+import io.kestra.core.models.flows.Flow;
+import io.kestra.jdbc.JdbcTestUtils;
+import io.micronaut.data.model.Pageable;
+import io.micronaut.data.model.Sort;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import jakarta.inject.Inject;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+
+public abstract class AbstractJdbcFlowRepositoryTest extends io.kestra.core.repositories.AbstractFlowRepositoryTest {
+    @Inject
+    AbstractFlowRepository flowRepository;
+
+    @Inject
+    JdbcTestUtils jdbcTestUtils;
+
+    @Test
+    void find() {
+        List<Flow> save = flowRepository.find(null, Pageable.from(1, 100, Sort.of(Sort.Order.asc("id"))));
+        assertThat((long) save.size(), is(Helpers.FLOWS_COUNT));
+
+        save = flowRepository.find("trigger-multiplecondition", Pageable.from(1, 10, Sort.UNSORTED));
+        assertThat((long) save.size(), is(3L));
+    }
+
+    @Test
+    void findSourceCode() {
+        List<SearchResult<Flow>> search = flowRepository.findSourceCode("io.kestra.core.models.conditions.types.MultipleCondition", Pageable.from(1, 10, Sort.UNSORTED));
+
+        assertThat((long) search.size(), is(1L));
+
+        SearchResult<Flow> flow = search
+            .stream()
+            .filter(flowSearchResult -> flowSearchResult.getModel()
+                .getId()
+                .equals("trigger-multiplecondition-listener"))
+            .findFirst()
+            .orElseThrow();
+        assertThat(flow.getFragments().get(0), containsString("types.MultipleCondition</mark>"));
+    }
+
+    @BeforeEach
+    protected void init() throws IOException, URISyntaxException {
+        jdbcTestUtils.drop();
+        jdbcTestUtils.migrate();
+        super.init();
+    }
+}